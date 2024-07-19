477fabd162fb5a2a9d502eb8e14bbcb407e46d21
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-07-19 09:06:14 +0900

    更新

--- src/UseEffect.tsx
+++ src/UseEffect.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 const App = () => {
   const [input1, setInput1] = useState('');
+  const [value, setValue] = useState(0);
   console.log('initialize...');
 
   const handleChange1 = (e) => {
@@ -9,13 +10,18 @@ const App = () => {
     setInput1(e.target.value);
   };
 
+  const handleClick = (e) => {
+    setValue(1);
+  };
+
   useEffect(() => {
     console.log('effect...レンダーのたびに毎回呼ばれる');
     return () => console.log('clean up...');
-  }, []);
+  }, [value]);
   return (
     <div>
       <input type="text" value={input1} onChange={handleChange1} />
+      <button onClick={handleClick}></button>
       hoge
     </div>
   );
